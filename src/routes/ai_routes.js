45f219837709ec67bb731e4d89a8c45f327eee6a
MinhPham485 <minh04082005@gmail.com>
2026-04-13 10:48:52 +0700

    refactor(ai): simplify group ask route chain

--- src/routes/ai_routes.js
+++ src/routes/ai_routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {verifyToken} = require('../middlewares/auth_middleware');
 const {isGroupMember} = require('../middlewares/group_middleware');
-const {validateAskQuestion, rateLimitAskQuestion} = require('../middlewares/ai_middleware');
+const {validateAskQuestion} = require('../middlewares/ai_middleware');
 const aiController = require('../controllers/ai_controllers');
 
-router.post('/group/:groupId/ask', verifyToken, validateAskQuestion, isGroupMember, rateLimitAskQuestion, aiController.askGroupAssistant);
+router.post('/group/:groupId/ask', verifyToken, validateAskQuestion, isGroupMember, aiController.askGroupAssistant);
 
 module.exports = router;
