0d93a92bc0fe9cde6b073f16f5f36c71fbdc2833
MinhPham485 <minh04082005@gmail.com>
2026-09-12 13:27:29 +0700

    chore : add argo application

--- infra/main.tf
+++ infra/main.tf
@@ -50,7 +50,7 @@ resource "google_compute_address" "vm_ip" {
 }
 resource "google_compute_instance" "vm" {
   name         = "task-manager-vm"
-  machine_type = "e2-small"
+  machine_type = "e2-medium"
   zone         = var.zone
 
   allow_stopping_for_update = true
